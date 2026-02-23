c22916f2bd148e52ef54964c6e75f7de045af159
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2026-02-23 18:34:26 +1000

    wip: start header

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -5,5 +5,5 @@ import { initThemeSwitch } from "./features/themeSwitch";
 document.addEventListener("DOMContentLoaded", () => {
   initAnimations();
   initLenis();
-  // initThemeSwitch();
+  initThemeSwitch();
 });
